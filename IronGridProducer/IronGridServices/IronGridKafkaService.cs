437699a899626a2195a4bee0f9a985472ce6a2b9
Pesach Bagad <bgdpsh@gmail.com>
2026-08-23 12:15:33 +0300

    add producer logic

---
+++ IronGridProducer/IronGridServices/IronGridKafkaService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+namespace IronGridProducer.IronGridServices;
+
+public class KafkaProducerServices
+{
+    private readonly string _bootstrapServices;
+    private readonly IProducer<Null, string> _producer;
+    public KafkaProducerServices(string bootstrapServices)
+    {
+        _bootstrapServices = bootstrapServices;
+        var config = new ProducerConfig { BootstrapServers = bootstrapServices };
+        _producer = new ProducerBuilder<Null, string>(config).Build();
+    }
+
+    public async Task SendAsync(string topicName, string content)
+    {
+
+        var message = new Message<Null, string> { Value = content };
+        var result = await _producer.ProduceAsync(topicName, message);
+    }
+}   
